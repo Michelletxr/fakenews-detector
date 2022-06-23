6ad6eb8f7d9d717efa27f2ca0490b8ee4456211b
michelle <michelleteixeiramartins@gmail.com>
2022-06-23 14:05:48 -0300

    melhorando o arq base

--- fakeNews_detector/src/main/java/br/ufrn/imd/model/service/PreProcessingImpl.java
+++
@@ -1,7 +0,0 @@
-package br.ufrn.imd.model.service;
-
-import br.ufrn.imd.model.service.PreProcessing;
-
-public class PreProcessingImpl implements PreProcessing {
-    private String message_processed;
-}
--- fakeNews_detector/src/main/java/br/ufrn/imd/model/service/SimilarityAnalysisImpl.java
+++
@@ -1,7 +0,0 @@
-package br.ufrn.imd.model.service;
-
-import br.ufrn.imd.model.service.SimilarityAnalysis;
-
-public class SimilarityAnalysisImpl implements SimilarityAnalysis {
-    private Double similarity_percent;
-}
