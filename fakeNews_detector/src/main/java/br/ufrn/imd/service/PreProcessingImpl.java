5e24d5409adb5acaabc2fdf452d7a5fc7763622f
victorbxavier <victorxavier530@gmail.com>
2022-07-03 22:51:24 -0300

    Nova deletagem desses arquivos.

--- fakeNews_detector/src/main/java/br/ufrn/imd/service/PreProcessingImpl.java
+++
@@ -1,5 +0,0 @@
-package br.ufrn.imd.service;
-
-public class PreProcessingImpl implements PreProcessing {
-    private String message_processed;
-}
--- fakeNews_detector/src/main/java/br/ufrn/imd/service/SimilarityAnalysisImpl.java
+++
@@ -1,5 +0,0 @@
-package br.ufrn.imd.service;
-
-public class SimilarityAnalysisImpl implements SimilarityAnalysis {
-    private Double similarity_percent;
-}
