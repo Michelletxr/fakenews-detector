05c6451c9761062fa1c8e778a890742df981244d
victorbxavier <victorxavier530@gmail.com>
2022-07-03 22:53:04 -0300

    Inclusão do parâmetro News.

--- fakeNews_detector/src/main/java/br/ufrn/imd/controller/NewsController.java
+++ fakeNews_detector/src/main/java/br/ufrn/imd/controller/NewsController.java
@@ -1,6 +1,9 @@
 package br.ufrn.imd.controller;
 
+import br.ufrn.imd.model.News;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
 public class NewsController {
+    News Noticia;
 }
