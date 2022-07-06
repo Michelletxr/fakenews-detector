6151cfd5502b3f40d0b45cc7d0e5eff4777e21f3
victorbxavier <victorxavier530@gmail.com>
2022-07-06 18:10:39 -0300

    Inclusão da dependência do commons do apache, para inclusão das bibliotecas jaroWinklerSimilarity e levenshtein.
    Atualização

--- fakeNews_detector/src/main/java/module-info.java
+++ fakeNews_detector/src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.example.fakenews_detector {
     requires javafx.controls;
     requires javafx.fxml;
             
-                        requires org.kordamp.bootstrapfx.core;
-            
+    requires org.kordamp.bootstrapfx.core;
+    requires org.apache.commons.text;
+
     opens br.ufrn.imd to javafx.fxml;
     exports br.ufrn.imd;
 }
